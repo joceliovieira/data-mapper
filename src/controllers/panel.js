7f3f4de4c5d045243fc556b5f1713401938a2a07
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-01-13 01:25:22 +0200

    Breaking Progress so far

--- src/controllers/panel.js
+++ src/controllers/panel.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const Excell = require('../services/excell.js');
+
 const router = express.Router();
+
+const excellReader = new Excell();
+
+router.use(fileUpload());
 
 //Main Panel Page
 router.get('/panel',function(req,res,next){
@@ -17,7 +24,23 @@ router.get('/data_assets',function(req,res,next){
 });
 
 router.post('/data_assets',function(req,res,next){
-  res.json({'csrfToken':req.csrfToken()});
+  const response={
+                  'status':false, //By Default we assume that the user uploaded a non Excell file
+                  'csrfToken':req.csrfToken() //Renew the CSRF token
+  };
+
+  const emmiter=excellReader.readAllLinesFromXLSXBufferAndProcessWithACallback(req.files.data_assets.data,(data,emmitter)=> {
+    console.log("Read Line: ",data);
+  });
+
+  emmiter.on('read_start',()=>{
+    response.status=true;
+    res.json(response);
+  });
+
+  emmiter.on('excell_read_error',()=>{
+    res.json(response);
+  });
 });
 
 router.get('/flow-map',function(req,res,next){
---
+++ src/services/excell.js
@@ -0,0 +1,56 @@
+const Excel = require('exceljs');
+const EventEmitter = require('events');
+const stream = require('stream');
+
+module.exports=function(emmiter=null) {
+
+  const self=this;
+
+  /**
+  * @var A private instance of the local event emmiter
+  */
+  let _emmiter=null;
+
+  //If no emmites has been provided create one
+  if(!emmiter){
+    _emmiter = new EventEmitter();
+  } else {
+    _emmiter=emmiter;
+  }
+
+  /**
+  * Read the excell workbook based on template and process the results using a callback.
+  *
+  * @param {Buffer} data The data from the uploaded excell file
+  * @param {Function} onReadCallback A callback that is used when the function reads an excell line.
+  *
+  * The callback should take the following parameters:
+  * lineData: An object containing the data of the line
+  * emmiter: An object where the event emmiter gets passed
+  *
+  * @return Emmiter
+  */
+  self.readAllLinesFromXLSXBufferAndProcessWithACallback = function(data,onReadCallback) {
+
+      const workbook= new Excell.Workbook();
+      const bufferStream = new stream.PassThrough();
+      bufferStream.end(data);
+      bufferStream.pipe(workbook.xlsx.createInputStream());
+
+      bufferStream.on('finish',()=>{
+        _emmiter.emmit('excell_read_start',worksheet.actualRowCount);
+        const worksheet = workbook.getWorksheet(1);
+        worksheet.eachRow((row, rowNumber) => {
+          if(rowNumber === 1){return;}//Skip first line
+          _emmiter.emmit('onRead',rowNumber,worksheet.actualRowCount);
+          console.log(row)
+          onReadCallback(row,_emmiter);
+        });
+      });
+
+      bufferStream.on('error',()=>{
+        _emmiter.emmit('excell_read_error',err);
+      });
+    return _emmiter;
+  };
+};
